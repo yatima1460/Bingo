e7e594fc7f845b6848b0e90464224587ef83e928
Federico Santamorena <federico@santamorena.me>
2019-09-19 05:49:17 +0200

    game background

--- BingoUI/Source/BingoUI.cpp
+++ BingoUI/Source/BingoUI.cpp
@@ -1,48 +1,101 @@
-// Example program:
-// Using SDL2 to create an application window
-
 #include <SDL.h>
 #include <cstdio>
 
-#undef main
 
+//Starts up SDL and creates window
+bool init();
 
+//Loads media
+bool loadMedia();
 
+//Frees media and shuts down SDL
+void close();
 
+//The window we'll be rendering to
+SDL_Window *gWindow = NULL;
 
+//The surface contained by the window
+SDL_Surface *gScreenSurface = NULL;
 
-int main(int argc, char* argv[])
-{
+//The image we will load and show on the screen
+SDL_Surface *gHelloWorld = NULL;
 
-    SDL_Window *window;                    // Declare a pointer
+bool init() {
+    //Initialization flag
+    bool success = true;
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-            "Bingo",                  // window title
-            SDL_WINDOWPOS_CENTERED,           // initial x position
-            SDL_WINDOWPOS_CENTERED,           // initial y position
-            1920,                               // width, in pixels
-            1080,                               // height, in pixels
-            SDL_WINDOW_OPENGL                 // flags - see below
-    );
-
-    // Check that the window was successfully created
-    if (window == nullptr) {
-        // In the case that the window could not be made...
-        fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
-        return 1;
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    } else {
+        //Create window
+        gWindow = SDL_CreateWindow("Bingo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, -1, -1,
+                                   SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP);
+        if (gWindow == nullptr) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        } else {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
     }
 
-    // The window is open: could enter program loop here (see SDL_PollEvent())
+    return success;
+}
 
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+bool loadMedia() {
+    //Loading success flag
+    bool success = true;
 
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
+    //Load splash image
+    gHelloWorld = SDL_LoadBMP("Assets/Fondo_Juego.bmp");
+    if (gHelloWorld == nullptr) {
+        printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp",
+               SDL_GetError());
+        success = false;
+    }
 
-    // Clean up
+    return success;
+}
+
+void close() {
+    //Deallocate surface
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = nullptr;
+
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+
+    //Quit SDL subsystems
     SDL_Quit();
+}
+
+#undef main
+
+int main(int argc, char *args[]) {
+    //Start up SDL and create window
+    if (!init()) {
+        printf("Failed to initialize!\n");
+    } else {
+        //Load media
+        if (!loadMedia()) {
+            printf("Failed to load media!\n");
+        } else {
+            //Apply the image
+            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+            //Update the surface
+            SDL_UpdateWindowSurface(gWindow);
+
+            //Wait two seconds
+            SDL_Delay(2000);
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
     return 0;
 }
